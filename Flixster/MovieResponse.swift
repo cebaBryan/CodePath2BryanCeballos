0517f57b7155aadb38087644c801362cfae3343f
cebaBryan <133716455+cebaBryan@users.noreply.github.com>
2024-02-04 22:32:21 -0500

    Update MovieResponse.swift

--- Flixster/MovieResponse.swift
+++ Flixster/MovieResponse.swift
@@ -1,5 +1,11 @@
-// MovieResponse.swift
+//
+//  MovieResponse.swift
+//  Flixster
+//
+//  Created by Bryan Ceballos on 2/4/24.
+//
 
+// APIService.swift
 import Foundation
 
 struct MovieResponse: Codable {
